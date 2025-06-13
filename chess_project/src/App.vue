cbd3bca122316ab9ac973df5bef0fe4d36bd8fbb
userName <userEmail>
2025-06-13 11:26:21 +0800

    Add chess_project as regular folder

---
+++ chess_project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+	margin: 0;
+	padding: 0;
+	background: #ffa600ab;
+}
+
+#app {
+	@include bfc;
+	position: relative;
+	margin: 0 auto;
+	min-height: 100%;
+	// background: url(./assets/bg.jpg) no-repeat;
+	background-position-x: center;
+	background-size: cover;
+}
+</style>
